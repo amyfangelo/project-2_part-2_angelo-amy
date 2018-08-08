4ede1d8a8520c7464e29c4fd7312b8712b646927
Amy F. Angelo <amyangelo1@me.com>
2018-08-07 20:22:27 -0400

    added h3 hover effect with jquery to about section

---
+++ js/animate.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("h3").hover(function(){
+        $(this).css("color", "#dbb1b5");
+
+        }, function(){
+        $(this).css("color", "#80a389");
+
+    });
+});
